499b4844e0f20aee64d419e448e2281831540450
onaug6th <592986151@qq.com>
2021-01-18 23:15:27 +0800

    [A] 项目初始化

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    quotes: [1, 'single'],
+    semi: 1,
+    '@typescript-eslint/camelcase': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    'no-irregular-whitespace': 2,
+    'no-case-declarations': 0,
+    'no-undef': 0,
+    'eol-last': 1,
+    'block-scoped-var': 2,
+    'comma-dangle': [2, 'never'],
+    'no-dupe-keys': 2,
+    'no-empty': 1,
+    'no-extra-semi': 2,
+    'no-multiple-empty-lines': [1, { max: 1, maxEOF: 1 }],
+    'no-trailing-spaces': 1,
+    'semi-spacing': [2, { before: false, after: true }],
+    'no-unreachable': 1,
+    'space-infix-ops': 1,
+    'spaced-comment': 1,
+    'no-var': 2,
+    'no-multi-spaces': 2,
+    'comma-spacing': 1,
+    'no-debugger': 0
+  }
+};
---
+++ vue.config.js
@@ -0,0 +1,57 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
+module.exports = {
+  productionSourceMap: false,
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'development') {
+      console.info(process.env.NODE_ENV);
+      config.devtool = 'eval-source-map';
+    } else {
+      config.optimization.minimizer[0].options.terserOptions.warnings = false;
+    }
+  },
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+      builderOptions: {
+        productName: 'IHelper',
+        appId: 'com.onaug6th.app',
+        copyright: 'onaug6th',
+        compression: 'store',
+        win: {
+          target: ['nsis', 'zip']
+        },
+        nsis: {
+          // 一键安装
+          oneClick: false,
+          // 注册表名
+          // guid: 'xxxx',
+          // 是否开启安装时权限限制（此电脑或当前用户）
+          perMachine: true,
+          // 允许请求提升。 如果为false，则用户必须使用提升的权限重新启动安装程序。
+          allowElevation: true,
+          // 允许修改安装目录
+          allowToChangeInstallationDirectory: true,
+          // 安装图标
+          // installerIcon: './build/icons/aaa.ico',
+          // 卸载图标
+          // uninstallerIcon: './build/icons/bbb.ico',
+          // 安装时头部图标
+          // installerHeaderIcon: './build/icons/aaa.ico',
+          // 创建桌面图标
+          createDesktopShortcut: true,
+          // 创建开始菜单图标
+          createStartMenuShortcut: true,
+          // 图标名称
+          shortcutName: 'iHelper'
+        }
+      }
+    },
+    'style-resources-loader': {
+      preProcessor: 'less',
+      // 引入全局样式变量
+      patterns: [path.resolve(__dirname, 'src/less/index.less')]
+    }
+  }
+};
