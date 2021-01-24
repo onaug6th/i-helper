2364bfd95ad02efd00e1d2027f62c78bed78a8b3
onaug6th <592986151@qq.com>
2021-01-24 22:46:48 +0800

    [A] 增加头部栏组件

---
+++ src/components/header/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <header class="header"></header>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import { onBeforeRouteUpdate, useRoute } from 'vue-router';
+
+export default defineComponent({
+  emits: ['option-click', 'on-close'],
+  setup(props, { emit }) {
+    props;
+    emit;
+
+    const currentRouteName = ref(useRoute().name);
+
+    onBeforeRouteUpdate((to, from, next) => {
+      currentRouteName.value = to.name;
+      next();
+    });
+
+    return {
+      currentRouteName
+    };
+  }
+});
+</script>
+
+<style lang="less" scoped>
+@import './index.less';
+</style>
