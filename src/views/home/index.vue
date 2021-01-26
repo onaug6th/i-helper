27e82dff6dd014b39a34b5174d07f49ccc40630f
onaug6th <592986151@qq.com>
2021-01-26 22:18:34 +0800

    [M] 主界面初始化

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,17 +1,64 @@
 <template>
   <Header />
+  <el-menu
+    default-active="1-4-1"
+    class="el-menu-vertical-demo"
+    @open="handleOpen"
+    @close="handleClose"
+    :collapse="true"
+  >
+    <el-submenu index="1">
+      <template #title>
+        <i class="el-icon-location"></i>
+        <span>导航一</span>
+      </template>
+      <el-menu-item-group>
+        <template #title>分组一</template>
+        <el-menu-item index="1-1">选项1</el-menu-item>
+      </el-menu-item-group>
+      <el-menu-item-group title="分组2">
+        <el-menu-item index="1-3">选项3</el-menu-item>
+      </el-menu-item-group>
+    </el-submenu>
+    <el-menu-item index="2">
+      <i class="el-icon-menu"></i>
+      <template #title>剪贴板历史</template>
+    </el-menu-item>
+    <el-menu-item index="3" disabled>
+      <i class="el-icon-document"></i>
+      <template #title>提醒功能</template>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <i class="el-icon-setting"></i>
+      <template #title>待办事项</template>
+    </el-menu-item>
+    <el-menu-item index="5">
+      <i class="el-icon-setting"></i>
+      <template #title>文件夹快速复制</template>
+    </el-menu-item>
+  </el-menu>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import Header from '@/components/header.vue';
+import { defineComponent, ref } from 'vue';
+import Header from '@/components/header/index.vue';
 
 export default defineComponent({
   components: {
     Header
   },
   setup() {
-    return {};
+    const isCollapse = ref(true);
+
+    return {
+      isCollapse,
+      handleOpen(key: string, keyPath: string) {
+        console.log(key, keyPath);
+      },
+      handleClose(key: string, keyPath: string) {
+        console.log(key, keyPath);
+      }
+    };
   }
 });
 </script>
