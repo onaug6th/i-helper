b4a0e45fa616c62001d751b46928a69a29edcc73
onaug6th <592986151@qq.com>
2021-01-28 22:12:41 +0800

    [M] 主面板样式及图标调整

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,46 +1,64 @@
 <template>
-  <Header />
-  <el-menu
-    default-active="1-4-1"
-    class="el-menu-vertical-demo"
-    @open="handleOpen"
-    @close="handleClose"
-    :collapse="true"
-  >
-    <el-submenu index="1">
-      <template #title>
-        <i class="el-icon-location"></i>
-        <span>导航一</span>
-      </template>
-      <el-menu-item-group>
-        <template #title>分组一</template>
-        <el-menu-item index="1-1">选项1</el-menu-item>
-      </el-menu-item-group>
-      <el-menu-item-group title="分组2">
-        <el-menu-item index="1-3">选项3</el-menu-item>
-      </el-menu-item-group>
-    </el-submenu>
-    <el-menu-item index="2">
-      <i class="el-icon-menu"></i>
-      <template #title>剪贴板历史</template>
-    </el-menu-item>
-    <el-menu-item index="3" disabled>
-      <i class="el-icon-document"></i>
-      <template #title>提醒功能</template>
-    </el-menu-item>
-    <el-menu-item index="4">
-      <i class="el-icon-setting"></i>
-      <template #title>待办事项</template>
-    </el-menu-item>
-    <el-menu-item index="5">
-      <i class="el-icon-setting"></i>
-      <template #title>文件夹快速复制</template>
-    </el-menu-item>
-  </el-menu>
+  <div class="home">
+    <Header />
+    <div class="home-content">
+      <div class="home-content__sidebar">
+        <el-menu
+          default-active="1-4-1"
+          class="el-menu-vertical-demo"
+          @open="handleOpen"
+          @close="handleClose"
+          :collapse="true"
+        >
+          <el-submenu index="1">
+            <template #title>
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item-group>
+              <template #title>分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-copy-document"></i>
+            <template #title>剪贴板</template>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <i class="el-icon-sunny"></i>
+            <template #title>提醒</template>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-date"></i>
+            <template #title>待办事项</template>
+          </el-menu-item>
+          <el-menu-item index="5">
+            <i class="el-icon-setting"></i>
+            <template #title>设置</template>
+          </el-menu-item>
+        </el-menu>
+      </div>
+      <div class="home-content__main">
+        <router-view v-slot="{ Component }">
+          <transition name="main-fade">
+            <div class="transition" :key="routeName">
+              <keep-alive>
+                <component :is="Component" />
+              </keep-alive>
+            </div>
+          </transition>
+        </router-view>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
+import { useRoute } from 'vue-router';
 import Header from '@/components/header/index.vue';
 
 export default defineComponent({
@@ -49,9 +67,11 @@ export default defineComponent({
   },
   setup() {
     const isCollapse = ref(true);
+    const routeName = ref(useRoute().name);
 
     return {
       isCollapse,
+      routeName,
       handleOpen(key: string, keyPath: string) {
         console.log(key, keyPath);
       },
@@ -63,4 +83,6 @@ export default defineComponent({
 });
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+@import './index.less';
+</style>
