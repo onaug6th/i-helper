f8f2ee340dfd230e90854a56810d4dd55c1fdaa0
onaug6th <592986151@qq.com>
2021-01-28 22:11:02 +0800

    [M] 修改头部组件样式

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -1,5 +1,13 @@
 <template>
-  <header class="header"></header>
+  <header class="header">
+    <div class="header-left"></div>
+    <div class="header-center"></div>
+    <div class="header-right">
+      <button class="icon flex-center close-window" @click="closeWindow" title="关闭">
+        <i class="iconfont flex-center icon-close"></i>
+      </button>
+    </div>
+  </header>
 </template>
 
 <script lang="ts">
@@ -7,7 +15,7 @@ import { defineComponent, ref } from 'vue';
 import { onBeforeRouteUpdate, useRoute } from 'vue-router';
 
 export default defineComponent({
-  emits: ['option-click', 'on-close'],
+  emits: ['option-click', 'close'],
   setup(props, { emit }) {
     props;
     emit;
@@ -19,8 +27,13 @@ export default defineComponent({
       next();
     });
 
+    const closeWindow = () => {
+      console.info(123);
+    };
+
     return {
-      currentRouteName
+      currentRouteName,
+      closeWindow
     };
   }
 });
