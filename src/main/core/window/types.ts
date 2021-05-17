17974d5dbf09cd60eeed2dcca2328e6cb207464d
onaug6th <onaug6th@qq.com>
2021-05-17 22:09:52 +0800

    feat: 又又又改设计了

--- src/main/core/window/types.ts
+++ src/main/core/window/types.ts
@@ -1,20 +1,18 @@
 //  创建窗口
-import { BrowserWindow, BrowserView } from 'electron';
+import { BrowserWindow } from 'electron';
 
 export interface WindowItem {
   //  窗体ID
   id: number;
-  //  窗体的类型
-  type: string;
+  //  插件ID
+  pluginId: string;
   //  窗体实例
   win: BrowserWindow;
-  //  窗体内的view实例
-  view?: BrowserView;
   //  窗体的父窗体id
   fatherId?: number;
 }
 
-export interface Windows {
+export interface PluginWin {
   [propName: number]: WindowItem;
 }
 
